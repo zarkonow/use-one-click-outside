293dd27adbb64d149672f8fcaf090cad46702b32
zarkonow <stojanov1987@gmail.com>
2024-11-10 19:57:37 +0100

    *first commit

---
+++ src/use-outside-click/test.jsx
@@ -0,0 +1,21 @@
+import { useState, useRef } from "react";
+import useOutsideClick from ".";
+
+export default function UseOneClickOutsideTest() {
+  const [showContent, setShowContent] = useState(false);
+  const ref = useRef();
+  useOutsideClick(ref, ()=>setShowContent(false));
+
+  return (
+    <div>
+      {showContent ? (
+        <div ref={ref}>
+          <h1>This is a random content</h1>
+          <p>Please click outside of this to close this.</p>
+        </div>
+      ) : (
+        <button onClick={() => setShowContent(true)}>Show Content</button>
+      )}
+    </div>
+  );
+}
